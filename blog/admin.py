03363e3319f1a39f128c2d873d10029823113e0d
MetinQardasov11 <metin.qardasov2003@gmail.com>
2025-08-11 22:45:20 +0400

    updated

--- blog/admin.py
+++ blog/admin.py
@@ -1,49 +1,19 @@
 from django.contrib import admin
 from .models import Blog, BlogImage, BlogCategory
-from modeltranslation.admin import TranslationAdmin, TabbedTranslationAdmin, TranslationStackedInline
+from modeltranslation.admin import TranslationAdmin
 
 @admin.register(BlogCategory)
-class BlogCategoryAdmin(TabbedTranslationAdmin):
+class BlogCategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'language', 'is_active', 'slug',)
     list_filter = ('language',)
-    class Media:
-        js = (
-            'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
-            'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
-            'modeltranslation/js/tabbed_translation_fields.js',
-        )
-        css = {
-            'screen': ('modeltranslation/css/tabbed_translation_fields.css',),
-        }
-    
 
 
 class BlogImageInline(admin.TabularInline):
     model = BlogImage
     extra = 1
-    class Media:
-        js = (
-            'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
-            'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
-            'modeltranslation/js/tabbed_translation_fields.js',
-        )
-        css = {
-            'screen': ('modeltranslation/css/tabbed_translation_fields.css',),
-        }
-    
     
 @admin.register(Blog)
-class BlogAdmin(TabbedTranslationAdmin):
+class BlogAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'slug')
     list_filter = ('category',)
     inlines = [BlogImageInline]
-    class Media:
-        js = (
-            'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
-            'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
-            'modeltranslation/js/tabbed_translation_fields.js',
-        )
-        css = {
-            'screen': ('modeltranslation/css/tabbed_translation_fields.css',),
-        }
-    
--- blog/translation.py
+++ blog/translation.py
@@ -1,10 +1,10 @@
 from modeltranslation.translator import TranslationOptions, register
 from .models import Blog, BlogCategory
 
-@register(BlogCategory)
-class BlogCategoryTranslationOptions(TranslationOptions):
-    fields = ('name',)
+# @register(BlogCategory)
+# class BlogCategoryTranslationOptions(TranslationOptions):
+#     fields = ('name',)
 
-@register(Blog)
-class BlogTranslationOptions(TranslationOptions):
-    fields = ('title', 'description', 'image', 'author')
+# @register(Blog)
+# class BlogTranslationOptions(TranslationOptions):
+#     fields = ('title', 'description', 'image', 'author')
