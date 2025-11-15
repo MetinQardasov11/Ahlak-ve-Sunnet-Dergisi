3662c4a40b8270f808e0dca3007bb04527599955
MetinQardasov11 <metin.qardasov2003@gmail.com>
2025-11-15 12:25:28 +0400

    Updated

--- base/urls.py
+++ base/urls.py
@@ -5,8 +5,8 @@ from .views import index, about, galeries, set_language, not_stared
 app_name = 'base'
 
 urlpatterns = [
-    # path('', index, name='index'),
-    path('', not_stared, name='index'),
+    path('', index, name='index'),
+    # path('', not_stared, name='index'),
     path('hakkimizda/', about, name='about'),
     path('galeri/', galeries, name='galeries'),
     path("set_language/<str:language>", set_language, name="set-language"),
